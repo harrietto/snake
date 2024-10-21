911e52585b857bb235e3d458946c8995bbec9cc7
Harriet <harriet.ilsley@gmail.com>
2024-10-21 22:01:30 +0100

    Use sprite groups

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import pygame
 
 GRID_LINES = (15, 15)
 SQUARE_SIZE = 30
-SCREEN_DIMENSIONS = (GRID_LINES[0] * SQUARE_SIZE + 800, GRID_LINES[1] * SQUARE_SIZE + 400)
-DIST_TO_GRID = ((SCREEN_DIMENSIONS[0] - GRID_LINES[0] * SQUARE_SIZE) / 2, (SCREEN_DIMENSIONS[1] - GRID_LINES[1] * SQUARE_SIZE) / 2)
 GRID_DIMENSIONS = (GRID_LINES[0] * SQUARE_SIZE, GRID_LINES[1] * SQUARE_SIZE)
+SCREEN_DIMENSIONS = (GRID_DIMENSIONS[0] + 800, GRID_DIMENSIONS[1] + 400)
+DIST_TO_GRID = ((SCREEN_DIMENSIONS[0] - GRID_DIMENSIONS[0]) / 2, (SCREEN_DIMENSIONS[1] - GRID_DIMENSIONS[1]) / 2)
 
 # How to draw the lines for the grid
 class Line(pygame.sprite.Sprite):
@@ -26,27 +26,23 @@ class Line(pygame.sprite.Sprite):
             self.rect.center = (position * SQUARE_SIZE + DIST_TO_GRID[0], SCREEN_DIMENSIONS[1] / 2)
 
 # Snake definition
-class Snake(pygame.sprite.Sprite):
+class SnakeHead(pygame.sprite.Sprite):
     
     # Draw the snake
-    def __init__(self):
+    def __init__(self, x, y):
         super().__init__()
         self.counter = 0
-        self.x = (GRID_LINES[0] + 1) // 2.5
-        self.y = (GRID_LINES[1] + 1) / 2
         self.direction = "right"
         self.image = pygame.Surface((SQUARE_SIZE, SQUARE_SIZE))
-        pygame.draw.rect(self.image, "#446b94", pygame.Rect(0, 0, SQUARE_SIZE, SQUARE_SIZE))
+        pygame.draw.rect(self.image, "#666b94", pygame.Rect(0, 0, SQUARE_SIZE, SQUARE_SIZE))
         self.rect = self.image.get_rect()
-        self.rect.center = (self.x * SQUARE_SIZE + DIST_TO_GRID[0] - SQUARE_SIZE / 2, self.y * SQUARE_SIZE + DIST_TO_GRID[1] - SQUARE_SIZE / 2)
+        self.rect.center = (x * SQUARE_SIZE + DIST_TO_GRID[0] - SQUARE_SIZE / 2, y * SQUARE_SIZE + DIST_TO_GRID[1] - SQUARE_SIZE / 2)
 
     # Moving the snake
     def update(self):
         self.counter += 1
-
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP and (self.direction == "left" or self.direction == "right"):
-                
                 self.direction = "up"
             if event.key == pygame.K_DOWN and (self.direction == "left" or self.direction == "right"):
                 self.direction = "down"
@@ -55,11 +51,10 @@ class Snake(pygame.sprite.Sprite):
             if event.key == pygame.K_RIGHT and (self.direction == "up" or self.direction == "down"):
                 self.direction = "right"
 
-        if self.counter < 10:
+        if self.counter < 20:
             return
         
         self.counter = 0
-
         if self.direction == "up":
             self.rect.y -= SQUARE_SIZE
         if self.direction == "down":
@@ -69,46 +64,67 @@ class Snake(pygame.sprite.Sprite):
         if self.direction == "right":
             self.rect.x += SQUARE_SIZE
 
+class SnakeSegment(pygame.sprite.Sprite):
+    
+    # Draw the snake
+    def __init__(self, x, y):
+        super().__init__()
+        self.counter = 0
+        self.image = pygame.Surface((SQUARE_SIZE, SQUARE_SIZE))
+        pygame.draw.rect(self.image, "#446b94", pygame.Rect(0, 0, SQUARE_SIZE, SQUARE_SIZE))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x * SQUARE_SIZE + DIST_TO_GRID[0] - SQUARE_SIZE / 2, y * SQUARE_SIZE + DIST_TO_GRID[1] - SQUARE_SIZE / 2)
+
 # Pygame setup
 pygame.init()
 screen = pygame.display.set_mode(SCREEN_DIMENSIONS)
 clock = pygame.time.Clock()
 pygame.display.set_caption("Snake")
-all_sprites = pygame.sprite.Group()
+grid_sprites = pygame.sprite.RenderPlain()
 
 # Add snake
-snake = Snake()
-all_sprites.add(snake)
+snake = pygame.sprite.RenderUpdates()
+x = (GRID_LINES[0] + 1) // 2.5
+y = (GRID_LINES[1] + 1) / 2
+snake_head = SnakeHead(x, y)
+snake.add(snake_head)
 
 # Add vertical lines for grid
-verticalLines = []
+vertical_lines = []
 for i in range(GRID_LINES[0] + 1):
-    verticalLines.append(Line(i, SQUARE_SIZE / 8, "vertical"))
-all_sprites.add(verticalLines)
+    vertical_lines.append(Line(i, SQUARE_SIZE / 8, "vertical"))
+grid_sprites.add(vertical_lines)
 
 # Add horizontal lines for grid
-horizontalLines = []
+horizontal_lines = []
 for i in range(GRID_LINES[1] + 1):
-    horizontalLines.append(Line(i, SQUARE_SIZE / 8, "horizontal"))
-all_sprites.add(horizontalLines)
+    horizontal_lines.append(Line(i, SQUARE_SIZE / 8, "horizontal"))
+grid_sprites.add(horizontal_lines)
 
-screen.fill("#222421")
+bg_surface = pygame.Surface(SCREEN_DIMENSIONS)
+pygame.draw.rect(bg_surface, "#222421", pygame.Rect(0, 0, SCREEN_DIMENSIONS[0], SCREEN_DIMENSIONS[1]))
+pygame.draw.rect(bg_surface, "#9cb087", pygame.Rect(DIST_TO_GRID[0], DIST_TO_GRID[1], GRID_DIMENSIONS[0], GRID_DIMENSIONS[1]))
+pygame.draw.circle(bg_surface, "#34cc23", (SCREEN_DIMENSIONS[0] / 2, SCREEN_DIMENSIONS[1] / 2), GRID_DIMENSIONS[0] / 2, width = 0)
+screen.blit(bg_surface, (0, 0))
 
+grid_sprites.draw(screen)
 
 running = True
+counter = 0
+
 while running:
+    counter += 1
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
+    snake.update()
 
-    all_sprites.update()
-
-    pygame.draw.rect(screen, "#9cb087", pygame.Rect(DIST_TO_GRID[0], DIST_TO_GRID[1], GRID_DIMENSIONS[0], GRID_DIMENSIONS[1]))
-
-    all_sprites.draw(screen)
-
-    pygame.display.flip()
+    snake.clear(screen, bgd=bg_surface)
+    snake.draw(screen, bg_surface)
+    grid_sprites.draw(screen)
+    pygame.display.update()
 
     clock.tick(60)
 
