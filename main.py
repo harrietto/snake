cb831e69b0527203dabd40bcb87941d39334dc52
Harriet <harriet.ilsley@gmail.com>
2024-11-24 13:49:33 +0000

    Add apple respawning

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class Line(pygame.sprite.Sprite):
         elif direction == "vertical":
             self.rect.center = (position * SQUARE_SIZE + DIST_TO_GRID[0], SCREEN_DIMENSIONS[1] / 2)
 
-class Snake(pygame.sprite.RenderUpdates):
+class Game(pygame.sprite.RenderUpdates):
 
     def __init__(self, x, y):
         super().__init__()
@@ -34,17 +34,21 @@ class Snake(pygame.sprite.RenderUpdates):
         self.direction = "right"
         self.next_direction = self.direction
         snake_head = SnakeHead(x, y)
-        segment_count = 7
+        segment_count = 10
         self.body = [SnakeSegment(x - count - 1, y) for count in range(segment_count)]
-        # self.body = [SnakeSegment(x - 1, y), SnakeSegment(x - 2, y), SnakeSegment(x - 3, y), SnakeSegment ]
         self.head = snake_head
         self.grid_filled = [[False for _ in range(GRID_LINES[1])] for _ in range(GRID_LINES[0])]
         self.grid_filled[int(x)][int(y)] = True
         self.grid_filled[int(x - 1)][int(y)] = True
         self.grid_filled[int(x - 2)][int(y)] = True
         self.grid_filled[int(x - 3)][int(y)] = True
+
         self.add(self.body)
         self.add(snake_head)
+
+        # Add apple
+        self.apple = Apple(self.grid_filled)
+        self.add(self.apple)
 
     # Moving the snake
     def update(self):
@@ -59,7 +63,7 @@ class Snake(pygame.sprite.RenderUpdates):
             if event.key == pygame.K_RIGHT and (self.direction == "up" or self.direction == "down"):
                 self.next_direction = "right"
 
-        if self.counter < 15:
+        if self.counter < 12:
             return
         
         self.counter = 0
@@ -72,13 +76,16 @@ class Snake(pygame.sprite.RenderUpdates):
         self.body.insert(0, last_segment)
 
         if self.direction == "up":
-            self.head.rect.y -= SQUARE_SIZE
+            self.head.move_by(0, -1)
         if self.direction == "down":
-            self.head.rect.y += SQUARE_SIZE 
+            self.head.move_by(0, 1)
         if self.direction == "left":
-            self.head.rect.x -= SQUARE_SIZE
+            self.head.move_by(-1, 0)
         if self.direction == "right":
-            self.head.rect.x += SQUARE_SIZE
+            self.head.move_by(1, 0)
+
+        if self.head.grid_x == self.apple.grid_x and self.head.grid_y == self.apple.grid_y:
+            self.apple.spawn(self.grid_filled)
 
 # Snake definition
 class SnakeHead(pygame.sprite.Sprite):
@@ -86,10 +93,17 @@ class SnakeHead(pygame.sprite.Sprite):
     # Draw the snake
     def __init__(self, x, y):
         super().__init__()
+        self.grid_x = x
+        self.grid_y = y
         self.image = pygame.Surface((SQUARE_SIZE, SQUARE_SIZE))
         pygame.draw.rect(self.image, "#525680", pygame.Rect(0, 0, SQUARE_SIZE, SQUARE_SIZE))
         self.rect = self.image.get_rect()
         self.rect.center = (x * SQUARE_SIZE + DIST_TO_GRID[0] - SQUARE_SIZE / 2, y * SQUARE_SIZE + DIST_TO_GRID[1] - SQUARE_SIZE / 2)
+
+    def move_by(self, i, j):
+        self.grid_x += i
+        self.grid_y += j
+        self.rect.center = (self.grid_x * SQUARE_SIZE + DIST_TO_GRID[0] - SQUARE_SIZE / 2, self.grid_y * SQUARE_SIZE + DIST_TO_GRID[1] - SQUARE_SIZE / 2)
 
 class SnakeSegment(pygame.sprite.Sprite):
     
@@ -106,15 +120,19 @@ class Apple(pygame.sprite.Sprite):
 
     def __init__(self, grid_filled):
         super().__init__()
+        self.spawn(grid_filled)
+
+    def spawn(self, grid_filled):
         available_squares = []
         for x in range(GRID_LINES[0]):
            for y in range(GRID_LINES[1]):
                if grid_filled[x][y] == False:
                    available_squares.append([x + 1, y + 1])
-        print(available_squares)
         self.apple_location = random.randint(0, len(available_squares) - 1)
         x = available_squares[self.apple_location][0]
         y = available_squares[self.apple_location][1]
+        self.grid_x = x
+        self.grid_y = y
         self.image = pygame.Surface((SQUARE_SIZE, SQUARE_SIZE))
         pygame.draw.rect(self.image, "#b53c42", pygame.Rect(0, 0, SQUARE_SIZE, SQUARE_SIZE))
         self.rect = self.image.get_rect()
@@ -130,12 +148,7 @@ grid_sprites = pygame.sprite.RenderPlain()
 # Add snake
 x = (GRID_LINES[0] + 1) // 2.5
 y = (GRID_LINES[1] + 1) / 2
-snake = Snake(x, y)
-
-# Add apple
-apple_draw = pygame.sprite.RenderPlain()
-apple = Apple(snake.grid_filled)
-apple_draw.add(apple)
+game = Game(x, y)
 
 # Add vertical lines for grid
 vertical_lines = []
@@ -166,12 +179,12 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
-    snake.update()
-    apple.update()
+    game.update()
+    #apple.update()
 
-    snake.clear(screen, bgd=bg_surface)
-    snake.draw(screen, bg_surface)
-    apple_draw.draw(screen, bg_surface)
+    game.clear(screen, bgd=bg_surface)
+    game.draw(screen, bg_surface)
+    #apple_draw.draw(screen, bg_surface)
     grid_sprites.draw(screen)
     pygame.display.update()
 
